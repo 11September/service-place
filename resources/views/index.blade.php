97b02bab605a8fa7777af788053ef682693cd654
Stanislav <shiptya@icloud.com>
2018-08-29 19:10:09 +0300

    add nesessary files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Создай свое обьявлене</title>
+    <link rel="icon" href="favicon.ico" type="image/ico">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/design.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+</head>
+<body>
+<header>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/parallax.min.js"></script>
+</header>
+<div class="container-fluid no-pad">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 no-pad">
+        <div class="hello-screen">
+            <button class="signin" type="button" name="button">
+                <a href="#">Sign In</a>
+            </button>
+            <h1 class="animated fadeInUp">SERVICE PLACE</h1>
+            <p class="animated fadeInUp">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <form action="" method="" class="animated fadeInUp flex">
+                <input class="search" type="search" placeholder="What are you looking for?">
+            </form>
+        </div>
+        <div class="search-filter">
+            <p>10 results "<span>Title</span>" </p>
+        </div>
+        <div class="item-list row no-marg">
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data row">
+                    <div class="col-lg-2 col-md-3 col-sm-3 col-xs-3">
+                        <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    </div>
+                    <div class="col-lg-10 col-md-9 col-sm-9 col-xs-9">
+                        <p class="serv-name">Oleg Gusev</p>
+                        <button type="button" name="button" class="message">Send message</button>
+                    </div>
+                </div>
+
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+            <div class="animated fadeInUp service col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                <p class="serv-bold">Title of text</p>
+                <p class="serv-prop">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="serv-data flex">
+                    <img class="serv-logo" src="./img/oleg.jpg" alt="">
+                    <p class="serv-name">Oleg Gusev</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
